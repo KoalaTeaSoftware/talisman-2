9bb8692b316ede167d6498225939eab0eaded50c
Mark Goldthorp <code@koalatea-software.com>
2020-08-19 10:18:21 +0100

    made deviceReady distinguish between browser and phone Added debug messages on screen

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,42 @@
-document.addEventListener('deviceready', onDeviceReady);
+showDebugMsg("loading up listeners");
+
+const sentinelsProductID = "com.earthoracles.celtmistic.premium_sentinels";
+
+document.addEventListener('deviceready', phoneDeviceReady);
+window.addEventListener('load', debugDeviceReady, false); // for debugging in browser
+
+function debugDeviceReady() {
+    showDebugMsg("onLoad was triggered");
+    onDeviceReady();
+};
+
+function phoneDeviceReady() {
+    showDebugMsg("deviceReady was triggered");
+    onDeviceReady();
+};
 
 function onDeviceReady() {
-  store.when('cc.fovea.purchase.consumable1')
-    .updated(refreshUI)
-    .approved(finishPurchase);
-  store.register({type: store.CONSUMABLE, id: 'cc.fovea.purchase.consumable1'});
-  store.refresh();
+    if ((typeof store) === 'undefined') {
+        showDebugMsg("Store is undefined")
+        return;
+    }
+    store.when(sentinelsProductID)
+        .updated(refreshUI)
+        .approved(finishPurchase);
+    store.register({type: store.NON_CONSUMABLE, id: sentinelsProductID});
+    store.refresh();
 }
 
 function finishPurchase(p) {
-  localStorage.goldCoins = (localStorage.goldCoins | 0) + 10;
-  p.finish();
+    localStorage.goldCoins = (localStorage.goldCoins | 0) + 10;
+    p.finish();
 }
 
 function refreshUI() {
-  const product = store.get('cc.fovea.purchase.consumable1');
-  const button = `<button onclick="store.order('cc.fovea.purchase.consumable1')">Purchase</button>`;
+    const product = store.get('cc.fovea.purchase.consumable1');
+    const button = `<button onclick="store.order(sentinelsProductID)">Purchase</button>`;
 
-  document.getElementsByTagName('body')[0].innerHTML = `
+    document.getElementById('display').innerHTML = `
   <div>
   <pre>
   Gold: ${localStorage.goldCoins | 0}
@@ -30,4 +49,9 @@ function refreshUI() {
   </pre>
   ${product.canPurchase ? button : ''}
   </div>`;
+}
+
+function showDebugMsg(msg) {
+    const block = document.getElementById("messages");
+    block.innerHTML += ("<br>" + msg);
 }
